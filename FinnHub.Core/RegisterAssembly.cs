7a7aaffaf0b9c6a1eff7b8a03975280a03c37662
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-03-03 17:05:05 -0500

    Adding environment variables

--- FinnHub.Core/RegisterAssembly.cs
+++ FinnHub.Core/RegisterAssembly.cs
@@ -4,41 +4,35 @@ using System;
 using System.Net;
 using System.Net.Http;
 using static Pineapple.Common.Preconditions;
+using static System.Environment;
 
 namespace FinnHub.Core
 {
     public static class RegisterAssembly
     {
         const string HTTPCLIENT_NAME = "FinnHubHttpClient";
-        // FinnHub API Key
-        const string API_KEY = "sandbox_c0mkopf48v6tkq132be0";
-        //current URL is https://finnhub.io/
-        const string BASE_URL = "https://finnhub.io/";
-        //current version is "/api/v1"
-        const string API_VERSION = "/api/v1";
-        const string FINNHUB_TOKEN_HEADER = "X-Finnhub-Token";
 
         public static void AddServices(IServiceCollection services, IConfiguration config)
         {
             CheckIsNotNull(nameof(services), services);
             CheckIsNotNull(nameof(config), config);
-
+            
             var settings = new FinnSettings
             {
-                ApiKey = API_KEY,
-                BaseURL = BASE_URL,
-                Version = API_VERSION,
-                UsePremiumOptions = true
+                ApiKey = GetEnvironmentVariable("ApiKey"),
+                BaseURL = GetEnvironmentVariable("FinnHubBaseUrl"),
+                Version = GetEnvironmentVariable("FinnHubApiVersion"),
+                UsePremiumOptions = bool.Parse(GetEnvironmentVariable("UsePremiumOptions"))
             };
 
             services.AddSingleton(settings);
             services.AddTransient<IFinnHubDependencies, FinnHubDependencies>();
-            services.AddTransient<FinnHubClient>();
+            services.AddTransient<IFinnHubClient, FinnHubClient>();
             services.AddTransient<BrotliCompressionHandler>();
             services.AddHttpClient(HTTPCLIENT_NAME, client =>
             {
                 client.BaseAddress = new Uri($"{settings.BaseURL}{settings.Version}");
-                client.DefaultRequestHeaders.Add(FINNHUB_TOKEN_HEADER, settings.ApiKey);
+                client.DefaultRequestHeaders.Add(GetEnvironmentVariable("FinnHubTokenHeader"), settings.ApiKey);
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
             }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
             {
